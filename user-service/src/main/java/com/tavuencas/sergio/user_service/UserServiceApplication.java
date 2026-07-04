9cb779255540cf89afca6767eb2f211c4d7437b7
Sergio Tavuencas <sergio_tavuencas@outlook.com>
2026-07-04 12:37:00 -0300

    User service

---
+++ user-service/src/main/java/com/tavuencas/sergio/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tavuencas.sergio.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
