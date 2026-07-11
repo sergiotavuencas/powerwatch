bcb678188065fc8a9d7ea733ed3f19bd24e50d9a
Sergio Tavuencas <sergio_tavuencas@outlook.com>
2026-07-11 17:39:38 -0300

    Ingestion energy usage simulation

--- ingestion-service/src/main/java/com/tavuencas/sergio/ingestion_service/dto/EnergyUsageRequestDto.java
+++ ingestion-service/src/main/java/com/tavuencas/sergio/ingestion_service/dto/EnergyUsageRequestDto.java
@@ -1,9 +1,11 @@
 package com.tavuencas.sergio.ingestion_service.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 
 import java.time.Instant;
 
+@Builder
 public record EnergyUsageRequestDto(
         Long deviceId,
         double energyConsumed,
