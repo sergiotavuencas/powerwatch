c9fe340c33ffc7e412b8b92b70f11a6c8869727a
Sérgio Vicente Tavuencas <sergiovicente_tavuenc@outlook.com>
2026-08-08 12:49:59 -0300

    Circuit breaker

--- api-gateway/src/main/java/com/tavuencas/sergio/api_gateway/route/DeviceServiceRoutes.java
+++ api-gateway/src/main/java/com/tavuencas/sergio/api_gateway/route/DeviceServiceRoutes.java
@@ -1,22 +1,40 @@
 package com.tavuencas.sergio.api_gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @Configuration
-public class DeviceServiceRoute {
+public class DeviceServiceRoutes {
     @Bean
     public RouterFunction<ServerResponse> deviceRoute() {
         return GatewayRouterFunctions.route("device-service")
                 .route(RequestPredicates.path("/api/v1/device/**"), http())
                 .before(uri("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "deviceServiceCircuitBreaker",
+                        URI.create("forward:/deviceFallbackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> deviceFallbackRoute() {
+        return GatewayRouterFunctions.route("deviceFallbackRoute")
+                .route(RequestPredicates.path("/deviceFallbackRoute"),
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Device service is down.")
+                )
                 .build();
     }
 }
--- api-gateway/src/main/java/com/tavuencas/sergio/api_gateway/route/IngestionServiceRoutes.java
+++ api-gateway/src/main/java/com/tavuencas/sergio/api_gateway/route/IngestionServiceRoutes.java
@@ -1,22 +1,40 @@
 package com.tavuencas.sergio.api_gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
 @Configuration
-public class IngestionServiceRoute {
+public class IngestionServiceRoutes {
     @Bean
     public RouterFunction<ServerResponse> ingestionRoute() {
         return GatewayRouterFunctions.route("ingestion-service")
                 .route(RequestPredicates.path("/api/v1/ingestion/**"), http())
                 .before(uri("http://localhost:8082"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "ingestionServiceCircuitBreaker",
+                        URI.create("forward:/ingestionFallbackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> ingestionFallbackRoute() {
+        return GatewayRouterFunctions.route("ingestionFallbackRoute")
+                .route(RequestPredicates.path("/ingestionFallbackRoute"),
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Ingestion service is down.")
+                )
                 .build();
     }
 }
